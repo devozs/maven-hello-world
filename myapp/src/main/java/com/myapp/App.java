148bbac4941a97954d7b7be84e539e1b47f945a5
devozs <oz.shemesh@datomize.com>
2023-12-15 19:44:12 +0200

    Adding simple JUnit

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,7 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        String name = args.length > 0 ? args[0] : "Anonymous";
-        System.out.printf("Hello World! %s", name);
+        String message = createMessage(args.length > 0 ? args[0] : null);
+        System.out.println(message);
     }
+
+    public static String createMessage(String name) {
+        return String.format("Hello World! %s", name == null? "Anonymous": name);
+    }
+
 }
