41db37785bb0b089bfd718a867894a43f24334df
devozs <oz.shemesh@datomize.com>
2023-12-16 02:12:35 +0200

    fix kind install

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -4,8 +4,10 @@ echo "Installing Helm 3"
 sudo curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
 echo "Installing Kind"
-sudo curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64
-sudo chmod +x ./kind
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
+chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
 echo "Installing kubectl"
